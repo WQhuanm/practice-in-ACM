8fa14b59645fea43f849915906313c3b9ec4c17c
WQhaunm <2942096269@qq.com>
2022-09-03 09:06:57 +0800

    Add files via upload

---
+++ 字符串统计/字符串统计/源.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+//  //方法一
+//int main()
+//{
+//	char arr[1000] = { 0 };
+//	while (scanf("%s", arr) != EOF)
+//	{
+//		int i = 0, a = 0;
+//		for (i = 0; i < 102; i++)
+//		{
+//			
+//			if (arr[i] >= 'A' && arr[i] <= 'Z' || arr[i] >= '0' && arr[i] <= '9')
+//				a++;
+//			arr[i] = 'q';
+//		}
+//		printf("%d\n", a);
+//		
+//	}
+//
+//	return 0;
+//}
+
+  ////方法二
+//int main()
+//{
+//	char arr[1000];
+//	while (scanf("%s", arr) != EOF)
+//	{int i=0,a=0;
+//	int b =  strlen(arr);
+//	for (i = 0; i < b; i++)
+//	{
+//		if (arr[i] >= 'A' && arr[i] <= 'Z' || arr[i] >= '0' && arr[i] <= '9')
+//			a++;
+//	}
+//	printf("%d\n", a);
+//
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
