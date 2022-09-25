b3f0e03ad9df8f878fa3f02273b3cf6368fc6b2a
WQhaunm <2942096269@qq.com>
2022-09-25 09:44:45 +0800

    Update 源.cpp

--- 阿牛的EOF牛肉串/阿牛的EOF牛肉串/源.cpp
+++ 阿牛的EOF牛肉串/阿牛的EOF牛肉串/源.cpp
@@ -9,9 +9,9 @@ int main() {
 		if (n >= 3) {
 			long long f1 = 3, f2 = 8, fn = 0;
 			for (int i = 3; i <= n; ++i) {
-				fn = 2 * (f1 + f2);
-				f1 = f2; f2 = fn;
-			}
+				fn = 2 * (f1 + f2);          //正难则反，当第n个不为o时，无要求，直接f2*2，当第n个为o时，第n-1个必须不为o，即f1*2得到的第n-1个一定不为o
+				f1 = f2; f2 = fn;         
+			}                                      
 			printf("%lld\n", fn);
 		}
 	}
